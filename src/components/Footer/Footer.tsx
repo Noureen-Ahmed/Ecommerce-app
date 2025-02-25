8c57f75134aa7a7db559d353977f7b1c9a690981
Mason <seifzellaban@gmail.com>
2025-02-25 18:28:33 +0200

    footer removed, not done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,13 +10,5 @@ export default function Footer() {
     // You can implement any side effects here
   }, []);
 
-  return (
-    <>
-      <h1>Footer</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ab eius dolorum
-        doloremque impedit nulla veritatis dolorem officia delectus?
-      </p>
-    </>
-  );
+  return <></>;
 }
